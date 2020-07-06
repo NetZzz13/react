6b2375dadbd65eea45392a7aea534196bf0e0729
Aliaksandr Nemtsau <Noboxxx@yandex.ru>
2020-07-06 22:14:07 +0300

    Updates

---
+++ precache-manifest.a69d88661bce95932da9cd583e0a4f46.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1f262ba5dad9f87cf36d3ccf4b2630",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ee22bd2e6de81eb34374",
+    "url": "/react/static/css/main.325ed97b.chunk.css"
+  },
+  {
+    "revision": "92ccb6198b857f0c9d44",
+    "url": "/react/static/js/2.24ea9398.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.24ea9398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee22bd2e6de81eb34374",
+    "url": "/react/static/js/main.7924e3c1.chunk.js"
+  },
+  {
+    "revision": "155942f0a220383f2ac1",
+    "url": "/react/static/js/runtime-main.b62ab703.js"
+  },
+  {
+    "revision": "a7acc74436c492b4b0bad2c9fa241084",
+    "url": "/react/static/media/loading.a7acc744.gif"
+  },
+  {
+    "revision": "94e148f1e5508edd16744283f0fc8bc8",
+    "url": "/react/static/media/logo.94e148f1.png"
+  },
+  {
+    "revision": "44330dbd1b68607423fba43ee3aad470",
+    "url": "/react/static/media/profile.44330dbd.png"
+  }
+]);
